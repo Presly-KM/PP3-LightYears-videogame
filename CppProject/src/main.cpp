6b6024c43fa6301634d721dab64029d9dbfaf4fa
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-11 21:36:34 +0200

    Connaitre la taille d'un array et un vector

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
+#include <iomanip>
+#include <vector>
 
 int main()
 {
-	std::string msg = "hello steve";  // sequence of characters
+	//array can hold elements of any type
+	//array do not change size
+	int IDs[] = { 1234, 1334, 1434 };
 
-	int age = 2023;
+	//{..} is called initializer list
+	std::vector<int> IDVec = { 1234, 1334, 1434 };
 
-	float tempreture = 72.6;
+	
+	std::cout << "this first one of IDs is: " << IDs[0] << std::endl;
+	std::cout << "this first one of IDVec is: " << IDVec[0] << std::endl;
 
-	char characterA = 'A';
-	char charactera = 'a';
-
-	bool lightIsOn = false;
-	bool boolFromNum = true;
-
-	std::cout << msg << " " << age << "the tempreture is: " << tempreture << std::endl;
-
-	std::cout << lightIsOn << std::endl;
-
-	std::cout << boolFromNum << std::endl;
-
-	std::cout << "the size of int is:" << sizeof(int) << std::endl;
-
+	std::cout << "the last one of IDs is: " << IDs[sizeof(IDs)/sizeof(int) -1] << std::endl;
+	std::cout << "this last one of IDVec is: " << IDVec.back() << std::endl;
 }
