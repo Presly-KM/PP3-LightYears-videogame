efbfd3d1e6a5da07f394a7ca3b53586d4d822a67
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-11 19:45:07 +0200

    string type

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-	std::cout << "hello world"; 
+	std::string msg = "hello steve";  // sequence of characters
+
+	int age = 2023;
+
+	float tempreture = 72.6;
+
+	std::cout << msg << " " << age << "the tempreture is: " << tempreture << std::endl;
+	 
 }
 
