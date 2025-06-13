4746997684e822073aae9e52c1d3cc30fb909375
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-13 19:21:14 +0200

    Input 1

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -2,48 +2,23 @@
 #include <iomanip>
 #include <vector>
 #include <map>
-
+#include <string>
 
 int main()
 {
-	//Arithmetic Operators 
-	//+,-, * , /, %
-	float a = 10;
-	float b = 10;
+	int age;
+	std::cout << "please type in your age: " << std::endl;
+	std::cin >> age;                            // cin permet de rentrer des input
 
-	float c = a + b;
-	float d = a - b;
-	float e = a * b;
-	float f = a / b;
+	std::cin.clear(); // to clear the error tell the buffer that everithing is okay
+	std::cin.ignore(10000000, '\n'); // ignore the next 10000000 amount of characters or until hit a new line
+	
+	std::string firstName;
+	std::string lastName;
+	std::cout << "what is your name: " << std::endl;
+	std::cin >> firstName;
+	std::cin >> lastName;
 
-	int g = 0.6; // assignement operator, if assign float to int, you lose precision, anything after the . will be descarded.
-
-	//float h = a % b; // gives the reminder
-
-	a += b; // the same as a = a + b;
-	a -= b; // the same as a = a - b
-
-	a /= b;
-	a *= b;
-
-	a++;
-	int h = --a; // the same as a-=1; or a = a + 1;
-	std::cout << h << std::endl;
-
-	++a; // the same as a+=1; or a = a + 1;
-	--a; // the same as a-=1; or a = a - 1;
-
-
-   bool aEqualb = a == b; // comparison
-   bool aNotEqualb = a != b; // comparison
-   bool aSmallerThanb = a <= b;
-   bool aBiggerThanb = a >= b;
-
-   bool bothTrue = aEqualb && aNotEqualb; //both has to be true ti be returning true.
-   bool eitherTrue = (aEqualb || aBiggerThanb) && aNotEqualb; // any trye will return true ; will be false if both are false. 
-
-   std::cout << eitherTrue << std::endl;
-
-
+	std::cout << "hello " << age << " years old " << firstName << " " << lastName << std::endl;
 }    
 
