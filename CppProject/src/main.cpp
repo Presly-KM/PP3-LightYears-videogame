926e5dfbc641d5fd67a60aa8f9cdd476647ca3a6
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-15 23:28:14 +0200

    mistake

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -7,7 +7,7 @@
 
 int main()
 {
-	std::vector<int> idsVec = { 1,2,3,4,5,7,8,9 }; // com test de commentaire vector/int idsVec vector utilitiy be careful cause
+	std::vector<int> idsVec = { 1,2,3,4,5,7,8,9 }; // com test de commentaire vector/int idsVec vector utilitiy be careful cause mistake
 	for (int i = 0; i < idsVec.size(); i++)
 	{
 		std::cout << idsVec[i] << std::endl;
