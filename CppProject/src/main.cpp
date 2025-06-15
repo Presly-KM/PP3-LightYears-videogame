cda69658d5933c48b77ceb9c43f78490bc6c8890
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-15 23:31:24 +0200

    i++ increment

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -8,7 +8,7 @@
 int main()
 {
 	std::vector<int> idsVec = { 1,2,3,4,5,7,8,9 }; // com test de commentaire vector/int idsVec vector utilitiy be careful cause mistake happen 
-	for (int i = 0; i < idsVec.size(); i++) // new line. IdsVec.size down  i < 0 for loop
+	for (int i = 0; i < idsVec.size(); i++) // new line. IdsVec.size down  i < 0 for loop i++
 	{
 		std::cout << idsVec[i] << std::endl;
 	}
