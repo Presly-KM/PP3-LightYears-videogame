e16c8910b38df4aa0e5f509edee619e9dc7c6ad7
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-15 23:23:19 +0200

    com

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -7,12 +7,10 @@
 
 int main()
 {
-	int ids[6] = {1,2,3,4,5};
-
-	//the same as for(int i = 0; i < .. ; i++)
-	for (int item : ids)   // For every item in the array we're printing out item  --> engros le code ci contre equivaut a une loop sur le tableau ci dessus. On affiche ici un par un les éléments du tableau. Probleme est que c'est imprécis car ne prend par en compte les index comme un for loop conventionnem 
-	{ 
-	   std::cout << item << std::endl;
-     }
+	std::vector<int> idsVec = { 1,2,3,4,5,7,8,9 };
+	for (int i = 0; i < idsVec.size(); i++)
+	{
+		std::cout << idsVec[i] << std::endl;
+	}
 }
 
