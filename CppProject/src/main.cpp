795bc1b3014bfca4e04f164a79e707c12940b5aa
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-15 23:29:03 +0200

    new line

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -7,8 +7,8 @@
 
 int main()
 {
-	std::vector<int> idsVec = { 1,2,3,4,5,7,8,9 }; // com test de commentaire vector/int idsVec vector utilitiy be careful cause mistake happen
-	for (int i = 0; i < idsVec.size(); i++)
+	std::vector<int> idsVec = { 1,2,3,4,5,7,8,9 }; // com test de commentaire vector/int idsVec vector utilitiy be careful cause mistake happen 
+	for (int i = 0; i < idsVec.size(); i++) // new line
 	{
 		std::cout << idsVec[i] << std::endl;
 	}
