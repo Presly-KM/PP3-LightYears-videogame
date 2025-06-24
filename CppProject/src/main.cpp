7f78ee99c265c6e6a24336f3fd362e2558555ccf
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-24 18:26:18 +0200

    boucle avce tableau : for (int item : ids)

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -7,10 +7,16 @@
 
 int main()
 {
-	int ids[] = { 1,2,3,4,5 };
-	for (int i = 0; i <= sizeof(ids)/sizeof(int); i++) 
+	int ids[6] = { 1,2,3,4,5 }; //the same as int ids[6] = {1,2,3,4,5,0}
+	for (int i = sizeof(ids) / sizeof(int) - 1; i >= 0; i--)
 	{
-		std::cout << ids[i] << std::endl;                  // La console affiche 1,2,3,4,5  car ici elle détermine le plus simplement possible, à l'aide de "sizeof(ids)/sizeof(int)", le nombre d'éléments au sein du tableau. Il y'en 5. C'est aussi simple que ça. CEPENDANT ATTENTION ! La console affiche 1,2,3,4,5 puis -858993460 a cause de la regle du "off by one" qui s'applique dans les array et les vector dans le cas ou l'opérateur est <=. Il faut mettre l'opérateur "<" genre " i < sizeof(ids)/sizeof(int)" pour que cela donne 1,2,3,4,5
+		//std::cout << ids[i] << std::endl;
+	}
+
+	//the same as for(int i = 0; i < sizeof(ids) / sizeof(int); i++)
+	//{std::cout << ids[i] << std::endl;}
+	for (int item : ids)                                        // autre manière d'incrementer sans les problemes  d'index et de off by one
+	{
+		std::cout << item << std::endl;                         // Il indique ici 1,2,3,4,5,0 ! En effet n'oublions pas que ids indique ici 6 éléments au sein du tableau (int ids[6])
 	}
 }
-
