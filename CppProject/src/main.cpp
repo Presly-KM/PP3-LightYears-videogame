8784a1b32bdfe9a600436e17846268ec573c1d89
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-24 17:59:35 +0200

    boucle avec tableau regle du off by one

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -10,7 +10,7 @@ int main()
 	int ids[] = { 1,2,3,4,5 };
 	for (int i = 0; i <= sizeof(ids)/sizeof(int); i++) 
 	{
-		std::cout << ids[i] << std::endl;                  // La console affiche 5 car ici elle détermine le plus simplement possible, à l'aide de "sizeof(ids)/sizeof(int)", le nombre d'éléments au sein du tableau. Il y'en 5. C'est aussi simple que ça 
+		std::cout << ids[i] << std::endl;                  // La console affiche 1,2,3,4,5  car ici elle détermine le plus simplement possible, à l'aide de "sizeof(ids)/sizeof(int)", le nombre d'éléments au sein du tableau. Il y'en 5. C'est aussi simple que ça. CEPENDANT ATTENTION ! La console affiche 1,2,3,4,5 puis -858993460 a cause de la regle du "off by one" qui s'applique dans les array et les vector dans le cas ou l'opérateur est <=. Il faut mettre l'opérateur "<" genre " i < sizeof(ids)/sizeof(int)" pour que cela donne 1,2,3,4,5
 	}
 }
 
