fa54bf779e52710ae8b47535e293eda49e1feca8
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-24 17:40:54 +0200

    boucle d'un tableau avec la méthode "sizeof(ids)/sizeof(int)"

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -7,13 +7,10 @@
 
 int main()
 {
-	//get the sum of the numbers from 1 to 100
-	int prix = 0;
-
-	//for loops keeps looping
-	for (int i = 0; i <= 100; i++) 
+	int ids[] = { 1,2,3,4,5 };
+	for (int i = 0; i <= sizeof(ids)/sizeof(int); i++) 
 	{
-	std::cout << prix << std::endl;
+		std::cout << ids[i] << std::endl;                  // La console affiche 5 car ici elle détermine le plus simplement possible, à l'aide de "sizeof(ids)/sizeof(int)", le nombre d'éléments au sein du tableau. Il y'en 5. C'est aussi simple que ça 
 	}
 }
 
