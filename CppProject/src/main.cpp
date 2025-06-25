6e00ebaf65e83888a9161cee60fd9e24979568e3
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-25 15:59:23 +0200

    enum class Mood Mood mood = Mood::Happy;

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -7,16 +7,16 @@
 
 int main()
 {
-	enum Mood
+	//enum are basically named intergers
+	enum class Mood
 	  {
-		// enum are basically named integers
 		Happy= - 1,  // defaults to 0
-		Angry= -2,  // defaults to the prebious one + 1
+		Angry= -2,   // defaults to the prebious one + 1
 		Sad = 100    // same...
 	  };
 
 	//creat a variable of the enum type Mood, and git it a value of Happy                 // Donc enum a la capacité de créer des types tout comme int est un type, tout comme char, tout comme float etc
-	Mood mood = Happy;
+	Mood mood = Mood::Happy;           // Nous avons modifié enum Mood (ligne 11) pour qu'il devienne enum class Mood cela aimplqiue qu'il devint un "strong enum type" et plus simplement un "enum type". Ce changement implique que désormais la valeur de Mood mood ne peut plus simplement etre "Happy" ou "Angry" ou "Sad", on maintenant dire  Mood::Happy;  
 
 	int moodInInt = mood;
 
