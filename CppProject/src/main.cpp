14dbdcff3840d8191762708ee8877a64862c6b4f
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-26 17:54:54 +0200

    // input error handling

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -15,10 +15,22 @@ int main()
 	// const mean that the value is not changeable. It is set to the value you give it.
 	const float CONVERSION = 1.609344;
 
-	std::cout << "Please type in the distance in Miles : \n>>>";
+	std::cout << "Please type in the distance in Miles : \n>>>";   
 	
-	float miles;
-	std::cin >> miles;
+	float miles=0;    //important d'initilaiser à 0 car sinon on siat pas comment l'ordinateur peut interpreter la variable de manière automatique
+	std::cin >> miles;                                       // Ici quand la console va s'afficher ce sera a moi donc de taper au sein meme de la console, la donnée que je souhaite convertir en kilos
 
-	std::cout << "The distance in kilometers is: " << miles * CONVERSION << std::endl;
-} 
+	// input error handling
+	while (!std::cin.good())  // Si qcch ne va pas. Si l'input n'est pas entré correctemement
+	{
+		std::cin.clear();    // supprime l'input qu'on a entré dans la console 
+		std::cin.ignore(std::numeric_limits<std:: streamsize>::max(), '\n'); // On définit la limite de scaractères qu'on est autorisées à entrer dans la console
+   
+	    std::cout << "Error Input! Please give a number:\n>>>";
+	    std::cin >> miles;
+	}
+ 
+	
+std::cout << "The distance in kilometers is: " << miles * CONVERSION << std::endl; // La distance en kilomètres est = miles x CONVERSION !
+
+}
