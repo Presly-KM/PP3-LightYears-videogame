23e95544e36d1891814f7d25c2d34737be70486a
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-26 17:07:23 +0200

    convert miles to kilometers

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -7,34 +7,14 @@
 
 int main()
 {
-	//enum are basically named intergers
-	enum class Mood
-	  {
-		Happy= - 1,  // defaults to 0
-		Angry= -2,   // defaults to the prebious one + 1
-		Sad = 100    // same...
-	  };
+	// convert miles to Kilometers 
+	// take input from the user, ask a distance in Miles 
+	// convert that distance to kilometres and cout the value
+	 
 
-	//creat a variable of the enum type Mood, and give it a value of Happy                 // Donc enum a la capacité de créer des types tout comme int est un type, tout comme char, tout comme float etc
-	Mood mood = Mood::Happy;           // Nous avons modifié enum Mood (ligne 11) pour qu'il devienne enum class Mood cela aimplqiue qu'il devint un "strong enum type" et plus simplement un "enum type". Ce changement implique que désormais la valeur de Mood mood ne peut plus simplement etre "Happy" ou "Angry" ou "Sad", on maintenant dire  Mood::Happy;   C'est comme si désormais on disait j'ai besoin de la partie "Happy" qui se trouve dans Mood. Plus seulement j'ai besoin de Happy
+	// const mean that the value is not changeable. It is set to the value you give it.
+	const int conversion = 1.609344;
 
-	int moodInInt = (int)mood;        // on converti mood en int
+	c
 
-	Mood moodFromInt = (Mood)-1;
-	
-	switch (moodFromInt)
-	{
-	case Mood::Happy:
-		std::cout << "Happy" << std::endl;
-		break;
-	case Mood::Angry:
-		std::cout << "Angry" << std::endl;
-		break;
-	case Mood::Sad:
-		std::cout << "Sad" << std::endl;
-		break;
-	default:
-		std::cout << "no mood" << std::endl;
-		break;
-	}
 } 
