583c3e6833c7ab29818aac3fee8cf7a2edba5b88
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-06-28 20:57:53 +0200

    fucniton avec paramtres composés

--- CppProject/src/main.cpp
+++ CppProject/src/main.cpp
@@ -5,8 +5,28 @@
 #include <string>
 #include <limits>
 
-int main()
+//creates a function called printHelloword
+void printHelloWord()
 {
-	// given a text, find the repeated word.
+	//body of the function
+	std::cout << "hello world" << std::endl;
+}
+
+void printMsg(std::string msg, bool newLine)  // the signature of a function
+{
+	std::cout << msg;
+	if (newLine)
+	{
+		std::cout << std::endl;
+	}
+}
+
+
+
+int main() //this is the entry point, where the program starts.
+{
+	printHelloWord(); // call the helloWorld smallest one
+
+	printMsg("hello David!", true);
 
 }
